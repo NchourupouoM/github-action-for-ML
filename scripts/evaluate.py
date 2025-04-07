0d3547c8d59cf2d29dce22e2ddac5af52341b6eb
NchourupouoM <nchourupouom04@gmail.com>
2025-04-07 10:40:18 +0100

    refactorisation du code dans deux scripts (train.py et evaluate.py)

---
+++ scripts/evaluate.py
@@ -0,0 +1,75 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import ConfusionMatrixDisplay, accuracy_score, confusion_matrix, f1_score, roc_auc_score, roc_curve
+import skops.io as sio
+from train import ChurnModelPipeline
+import pickle
+
+class EvaluateChurnModel:
+    def __init__(self, model_trained):
+       self.model_pipeline = model_trained
+       self.X_test, self.y_test = ChurnModelPipeline.load_and_prepare_dataset()
+
+    def evaluate_model(self):
+        print("Evaluating the model...")
+        predictions = self.model_pipeline.predict(self.X_test)
+        accuracy = accuracy_score(self.y_test, predictions)
+        f1 = f1_score(self.y_test, predictions, average='macro')
+        print(f"Accuracy: {round(accuracy*100,2)}%, F1 score: {round(f1, 2)}")
+
+        return accuracy, f1 
+    def plot_confusion_matrix(self):
+        print("ploting confusion matrix...")
+        prediction = self.model_pipeline.predict(self.X_test)
+        cm = confusion_matrix(self.y_test, prediction, labels=self.model_pipeline.classes_)
+        disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=self.model_pipeline.classes_)
+        disp.plot()
+        plt.savefig("model_result.png", dpi=120)
+        print("Confusion matrix saved as model_results.png")
+
+    def save_matrics(self, accuracy, f1):
+        print("saving metrics to file...")
+        with open("metrics/metrics.txt", "w") as outfile:
+            outfile.write(f"accuracy = {round(accuracy, 2)}, \n F1 score: {round(f1, 2)}\n")
+        print("Metrics saved in metrics.txt")
+
+    def plot_roc_curve(self):
+        """Plot and save ROC curve for the classifier."""
+        print("Plotting ROC curve...")
+        y_probs = self.model_pipeline.predict_proba(self.X_test)[:, 1]  # Probabilities for class 1
+        fpr, tpr, _ = roc_curve(self.y_test, y_probs)
+
+        plt.figure(figsize=(8, 6))
+        plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC curve (area = {roc_auc_score(self.y_test, y_probs):.2f})')
+        plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Receiver Operating Characteristic (ROC) Curve')
+        plt.legend(loc="lower right")
+        plt.savefig("roc_curve.png", dpi=120)
+        print("ROC curve saved as 'roc_curve.png'.")
+
+    def save_pipeline(self):
+        print("saving pipeline to file...")
+        sio.dump(self.model_pipeline, "models/churn_pipeline.skops")
+        print("Pipeline saved as 'churn_prediction.skops'.")
+
+if __name__ == "__main__":
+
+    # Pour charger le modèle ultérieurement :
+    filename = "models/modele_logistic.pkl"
+    with open(filename, 'rb') as file:
+        model = pickle.load(file)
+
+    evaluate_model = EvaluateChurnModel(model_trained = model)
+
+    accuracy, f1 = evaluate_model.evaluate_model()
+
+    #plot CM and saved the metrics 
+    evaluate_model.plot_confusion_matrix()
+    evaluate_model.save_matrics(accuracy=accuracy, f1=f1)
+    evaluate_model.plot_roc_curve()
+    # save the pipeline 
+    evaluate_model.save_pipeline()
--- scripts/train.py
+++ scripts/train.py
@@ -13,6 +13,8 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, OrdinalEncoder
 from sklearn.linear_model import LogisticRegression 
 
+import pickle
+
 class ChurnModelPipeline:
     def __init__(self, data_path, target_column, index_column=None, random_state=125):
         self.data_path = data_path
@@ -35,6 +37,7 @@ class ChurnModelPipeline:
             X, y, test_size=0.3, random_state=self.random_state
         )
         print("Data load and split successfully.")
+        return self.X_test, self.y_test
 
     def build_processor(self, cat_cols, num_cols):
         print("building preprocessing pipeline...")
@@ -81,57 +84,17 @@ class ChurnModelPipeline:
             raise ValueError("Model pipeline is not initiate. Build the model pipeline first")
         print("Training the model...")
         self.model_pipeline.fit(self.X_train, self.y_train)
-        print("Model training completed.")
-    
-    def evaluate_model(self):
-        print("Evaluating the model...")
-        predictions = self.model_pipeline.predict(self.X_test)
-        accuracy = accuracy_score(self.y_test, predictions)
-        f1 = f1_score(self.y_test, predictions, average='macro')
-        print(f"Accuracy: {round(accuracy*100,2)}%, F1 score: {round(f1, 2)}")
 
-        return accuracy, f1 
-    def plot_confusion_matrix(self):
-        print("ploting confusion matrix...")
-        prediction = self.model_pipeline.predict(self.X_test)
-        cm = confusion_matrix(self.y_test, prediction, labels=self.model_pipeline.classes_)
-        disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=self.model_pipeline.classes_)
-        disp.plot()
-        plt.savefig("model_result.png", dpi=120)
-        print("Confusion matrix saved as model_results.png")
+        # Enregistrer le modèle dans un fichier
+        filename = 'models/modele_logistic.pkl'
+        with open(filename, 'wb') as file:
+            pickle.dump(self.model_pipeline, file)
 
-    def save_matrics(self, accuracy, f1):
-        print("saving metrics to file...")
-        with open("metrics.txt", "w") as outfile:
-            outfile.write(f"accuracy = {round(accuracy, 2)}, F1 score: {round(f1, 2)}\n")
-        print("Metrics saved in metrics.txt")
-
-    def plot_roc_curve(self):
-        """Plot and save ROC curve for the classifier."""
-        print("Plotting ROC curve...")
-        y_probs = self.model_pipeline.predict_proba(self.X_test)[:, 1]  # Probabilities for class 1
-        fpr, tpr, _ = roc_curve(self.y_test, y_probs)
-
-        plt.figure(figsize=(8, 6))
-        plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC curve (area = {roc_auc_score(self.y_test, y_probs):.2f})')
-        plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
-        plt.xlim([0.0, 1.0])
-        plt.ylim([0.0, 1.05])
-        plt.xlabel('False Positive Rate')
-        plt.ylabel('True Positive Rate')
-        plt.title('Receiver Operating Characteristic (ROC) Curve')
-        plt.legend(loc="lower right")
-        plt.savefig("roc_curve.png", dpi=120)
-        print("ROC curve saved as 'roc_curve.png'.")
-
-    def save_pipeline(self):
-        print("saving pipeline to file...")
-        sio.dump(self.model_pipeline, "churn_pipeline.skops")
-        print("Pipeline saved as 'churn_prediction.skops'.")
+        print("Model training completed and saved successfuly.")
 
 if __name__ == "__main__":
     #configuration
-    data_file = "Churn_Modelling.csv"
+    data_file = "data/Churn_Modelling.csv"
     target_col = "Exited"
     drop_cols = ["RowNumber","CustomerId","Surname"]
     
@@ -146,12 +109,4 @@ if __name__ == "__main__":
 
     # Train and evaluate model 
     churn_pipeline.train_model()
-    accuracy, f1 = churn_pipeline.evaluate_model()
-
-    #plot CM and saved the metrics 
-    churn_pipeline.plot_confusion_matrix()
-    churn_pipeline.save_matrics(accuracy=accuracy, f1=f1)
-    churn_pipeline.plot_roc_curve()
-    # save the pipeline 
-    churn_pipeline.save_pipeline()
 
